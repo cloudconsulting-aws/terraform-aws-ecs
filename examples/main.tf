8b01e7b4f50c219e9e40bfb714058cadb77e1bed
Asaph Tinoco <asaph@sendwave.com>
2025-09-04 18:33:57 +0200

    adding local variable

--- examples/main.tf
+++ examples/main.tf
@@ -3,8 +3,8 @@ module "ecs" {
   cluster_name             = "example-ecs-cluster"
   execution_role_arn       = "arn:aws:iam::123456789012:role/random-ecs-execution-role"
   task_role_arn            = "arn:aws:iam::123456789012:role/random-ecs-task-role"
-  container_definitions    = local.container_definitions 
-  network_mode             = "awsvpc" 
+  container_definitions    = local.container_definitions
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   task_cpu                 = "256"
   task_memory              = "512"
